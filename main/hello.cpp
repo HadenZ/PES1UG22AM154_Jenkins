478033d22547de888cd32d0c05270d5e03202a53
HadenZ <shrihari163@gmail.com>
2025-03-10 16:17:34 +0530

    CC lab commit for Task 4 - part 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this after doing Task 4  \n";
+  cout << "CC lab Task 4 check  \n";
   return 0;
 }
